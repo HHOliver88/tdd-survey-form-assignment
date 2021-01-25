cd44dc5a74cc2b8d9fb91921e5e1489d14b82560
Oliver Rosales <oliverjohnpr2013@gmail.com>
2021-01-25 11:07:25 +0800

    Added sample answer and a passed test.

--- models/survey.model.js
+++ models/survey.model.js
@@ -7,7 +7,32 @@ class SurveyModel extends Model {
 		super();
 	}
 
-	async getSurvey() {}
+	async getSurvey() {
+		let response_data 	    = {status: false, result: [], err: null};
+		
+		try{
+			let get_survey_query 		= Mysql.format(`
+				SELECT surveys.id, surveys.name, surveys.location, surveys.favorite_language, surveys.comment
+				FROM surveys
+				ORDER BY surveys.id DESC
+				LIMIT 1;`
+			);
+
+			let [get_survey_result] = await this.executeQuery(get_survey_query);
+	
+			if(get_survey_result){
+				response_data.status 	= true;
+				response_data.result 	= get_survey_result;
+			}else{
+				response_data.message 	= "No survey found";
+			}
+		}catch(err){
+			response_data.err 			= err;
+			response_data.message 		= "Error in getting a survey.";
+		};
+	
+		return response_data;		
+	}
 
 	async createSurvey(){}
 
--- test/survey.test.js
+++ test/survey.test.js
@@ -1,4 +1,28 @@
+process.env.NODE_ENV = 'test';
+
 const chai                  = require('chai');
+const expect                = chai.expect;
+const Connection            = require('../config/database');
+const SurveyModel           = require('../models/survey.model');
 
 
+describe("Survey Model", function(){
+
+    it('Should return expected data when no survey is found.', async function(){
+       
+        let surveyModel = new SurveyModel();
+        let data = await surveyModel.getSurvey();
+
+        /* Check and expected data.status is equal to true */
+        expect(data.status).to.equal(false);
+
+        /* Check and expected data.result.email is equal to email */
+        expect(data.message).to.equal("No survey found");
+    });
+
+    after(function(){
+        /* close DB connection */ 
+        Connection.end();
+    });
+});
 // Write your test cases here. Make sure you write the test first, before writing any code.
